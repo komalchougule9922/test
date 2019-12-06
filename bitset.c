16ab1605810d2d66f96083bc8c8c1c3e07c16509
komalchougule9922 <58587947+komalchougule9922@users.noreply.github.com>
2019-12-06 14:11:24 +0530

    Create bitset.c

---
+++ bitset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int bit_set(int x,int ps)
+{
+    return (x |(1<<ps));
+}
+
+int main()
+{
+     int Data=0x00;
+ int ps =0;
+
+    printf("Data = 0x%x\n\n",Data);
+    printf("Enter the position to set a bit  = ");
+    scanf("%d",&ps);
+
+    Data = bit_set(Data,ps);
+printf("\n\n%dth Bit Set Now Data will be = 0x%x\n",ps,Data);
+
+    return 0;
+}
