16b5ffc11040c08a1e64701562f25afe09e53f2c
komalchougule9922 <58587947+komalchougule9922@users.noreply.github.com>
2019-12-06 14:35:32 +0530

    Update bitset.c

--- bitset.c
+++ bitset.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
-int bit_set(int x,int ps)
-{
-    return (x |(1<<ps));
+int bit_set()
+{     
+    int x;
+    int ps;
+    return (x |(1<<ps));   //for set a perticular bit
+    // return (x &(~(1<<ps));   //for reset bit
 }
 
 int main()
 {
-     int Data=0x00;
+     int Data=00;
       int ps =0;
-
+   
     printf("Data = 0x%x\n\n",Data);
     printf("Enter the position to set a bit  = ");
     scanf("%d",&ps);
 
     Data = bit_set(Data,ps);
-printf("\n\n%dth Bit Set Now Data will be = 0x%x\n",ps,Data);
+    printf("\n\n%dth Bit Set Now Data will be = 0x%x\n",ps,Data);
 
     return 0;
 }
